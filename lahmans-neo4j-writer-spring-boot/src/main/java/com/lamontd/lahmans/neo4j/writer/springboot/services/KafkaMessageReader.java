58ca6769283891c33c07c30698d3f3dcb2dba199
LamontD <lamont.dozier.jr@gmail.com>
2020-05-23 21:36:42 -0400

    Updated the Kafka message reader to handle transaction processing time

--- lahmans-neo4j-writer-spring-boot/src/main/java/com/lamontd/lahmans/neo4j/writer/springboot/services/KafkaMessageReader.java
+++ lahmans-neo4j-writer-spring-boot/src/main/java/com/lamontd/lahmans/neo4j/writer/springboot/services/KafkaMessageReader.java
@@ -1,7 +1,17 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright 2020 lamontdozierjr.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package com.lamontd.lahmans.neo4j.writer.springboot.services;
 
@@ -13,6 +23,8 @@ import com.lamontd.utils.jackson.JacksonMapper;
 import com.lamontd.utils.transport.MappedTransportObject;
 import com.lamontd.utils.transport.StorageException;
 import com.lamontd.utils.transport.TransportConversionException;
+import java.time.Duration;
+import java.time.Instant;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -27,40 +39,42 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class KafkaMessageReader {
+
     private static final Log logger = LogFactory.getLog(KafkaMessageReader.class);
-    
+
     @Autowired
     private TransportObjectHandler transportObjectHandler;
     @Autowired
     private ComponentTransactionKafkaSender transactionSender;
-    
+
     public KafkaMessageReader() {
         logger.info("I have the power of Greyskull!");
     }
-    
-    private void handleMessage(String key, String message) {        
+
+    private void handleMessage(String key, String message) {
         try {
+            Instant start = Instant.now();
             final ObjectMapper jacksonMapper = JacksonMapper.getStandardMapper();
             MappedTransportObject incomingObject = jacksonMapper.readValue(message, MappedTransportObject.class);
-            
+
             if (StringUtils.isEmpty(incomingObject.getObjectType())) {
                 logger.warn("Received unexpected message that is not a TransportObject: " + message);
             } else if (incomingObject.getAttributes() == null || incomingObject.getAttributes().isEmpty()) {
                 logger.warn("Found message of type " + incomingObject.getObjectType() + " but no attributes");
             } else {
                 try {
-                boolean handledSuccessfully = transportObjectHandler.process(incomingObject);
-                if (StringUtils.isNotEmpty(incomingObject.getTransactionId())) {
-                    if (handledSuccessfully) {
-                        transactionSender.publishAck(incomingObject.getTransactionId());
-                    } else {
-                        transactionSender.publishNak(incomingObject.getTransactionId());
+                    boolean handledSuccessfully = transportObjectHandler.process(incomingObject);
+                    if (StringUtils.isNotEmpty(incomingObject.getTransactionId())) {
+                        if (handledSuccessfully) {
+                            transactionSender.publishAck(incomingObject.getTransactionId(), Duration.between(start, Instant.now()));
+                        } else {
+                            transactionSender.publishNak(incomingObject.getTransactionId(), Duration.between(start, Instant.now()));
+                        }
                     }
-                }
                 } catch (TransportConversionException | StorageException ex) {
                     logger.error("Failed to convert and storage incpoming transport object", ex);
                     if (StringUtils.isNotEmpty(incomingObject.getTransactionId())) {
-                        transactionSender.publishNak(incomingObject.getTransactionId());
+                        transactionSender.publishNak(incomingObject.getTransactionId(), Duration.between(start, Instant.now()));
                     }
                 }
             }
@@ -70,13 +84,13 @@ public class KafkaMessageReader {
             logger.error("Could not process incoming message -> " + message, unsup);
         }
     }
-    
+
     @Bean
     public java.util.function.Consumer<KStream<String, String>> process() {
-        return input ->
-                input.foreach((key, value) -> {
-                   handleMessage(key, value);
+        return input
+                -> input.foreach((key, value) -> {
+                    handleMessage(key, value);
                 });
     }
-    
+
 }
